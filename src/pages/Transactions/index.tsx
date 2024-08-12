bbae8f08b85b57d4c1a88e0d12fefd5999f1e2ef
Marcos Parreiras <marcosalp1998@gmail.com>
2024-08-12 00:16:57 -0300

    feat: add summary component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
 
 export function Transactions() {
   return (
     <h1>
       <Header />
+      <Summary />
     </h1>
   );
 }
