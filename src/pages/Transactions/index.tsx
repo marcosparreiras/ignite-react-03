905a989b14897d3019c71e260010a96cb28093da
Marcos Parreiras <marcosalp1998@gmail.com>
2024-08-12 22:53:38 -0300

    feat: add transanctions table on transactions page

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,38 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
-    <h1>
+    <div>
       <Header />
       <Summary />
-    </h1>
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>12/08/2024</td>
+            </tr>
+            <tr>
+              <td width="40%">Festa</td>
+              <td>
+                <PriceHighLight variant="outcome">R$ -500,00</PriceHighLight>
+              </td>
+              <td>Compra</td>
+              <td>12/08/2024</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
   );
 }
